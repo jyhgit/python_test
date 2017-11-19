0db90921e52afa3530e7345356591b5519c414d7
jiayanhua <1808996884@qq.com>
2017-11-19 20:21:57 +0800

    数据库操作

--- django_08day11/web/views.py
+++ django_08day11/web/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from 
 # Create your views here.
 def login():
     return "login"
